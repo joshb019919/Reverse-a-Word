4afff3bde67b39283a3f5879285bebb9fbbad3e4
Josh Borthick <46645462+joshb019919@users.noreply.github.com>
2022-07-16 14:37:31 -0500

    Split one method into two

--- wordReverse.java
+++ wordReverse.java
@@ -1,14 +1,17 @@
 class Main {
-  
-  public static void main(String[] args) {
-    String s = "The weather is amazing today!";
+  private static String reverse(String s) {
     String[] sArr = s.split(" ");
     String newS = "";
 
     for (int i = sArr.length-1; i > -1; --i) {
       newS = newS.concat(sArr[i]).concat(" ");
     }
+    
+    return newS;
+  
+  public static void main(String[] args) {
+    String s = "The weather is amazing today!";
 
-    System.out.println(newS);
+    System.out.println(reverse(s));
   }
 }
